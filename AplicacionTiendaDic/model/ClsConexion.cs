ceaa616aae26e75ce79dacb9663301b3b9770a77
Daniel Valenzuela <hetzondaniel1@gmial.com>
2023-12-13 20:59:35 -0600

    primer commit y conexion exitosa base de datos

---
+++ AplicacionTiendaDic/model/ClsConexion.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionTienda.model
+{
+    internal class ClsConexion
+    {
+
+        MySqlConnection conex = new MySqlConnection();
+
+        static string servidor = "localhost";
+        static string bd = "proyectodb";
+        static string usuario = "root";
+        static string password = "toor";
+        static string puerto = "3306";
+
+        string cadenaConexion = "server=" + servidor + ";" + "port=" + puerto + ";" + "user id=" + usuario + ";" + "password=" + password + ";" + "database=" + bd + ";";
+        public MySqlConnection establecerConexion()
+        {
+            try
+            {
+                conex.ConnectionString = cadenaConexion;
+                conex.Open();
+                MessageBox.Show("se logro conectar a la base de datos CORRECTAMENTE");
+
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show("no se pudo conectar a la base de datos, error: " + e);
+
+            }
+            return conex;
+        }
+    }
+}
